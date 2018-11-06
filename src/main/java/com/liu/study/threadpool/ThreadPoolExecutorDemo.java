e2fd619967b73ec4f855f03e7e5b1b0bf04c8fe4
liujinchengjx <liujinchengjx@163.com>
2018-11-06 19:13:29 +0800

    2

--- src/main/java/com/liu/study/threadpool/ThreadPoolExecutorDemo.java
+++ src/main/java/com/liu/study/threadpool/ThreadPoolExecutorDemo.java
@@ -5,6 +5,11 @@ import java.util.concurrent.*;
 class MyThread implements Runnable{
     public void run() {
         System.out.println(Thread.currentThread()+"===run");
+        try {
+            Thread.sleep(2000);
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }
 
 
     }
@@ -21,12 +26,28 @@ public class ThreadPoolExecutorDemo {
 
     public  static  void main(String[] args) throws  Exception{
        // ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(10,10,3000);
-        ExecutorService executorService = Executors.newFixedThreadPool(10);
+        long start = System.currentTimeMillis();
+        ExecutorService executorService = Executors.newFixedThreadPool(4);
         executorService.execute(new MyThread());
 
         MyCallable myCallable = new MyCallable();
+        MyCallable myCallable2 = new MyCallable();
+        MyCallable myCallable3 = new MyCallable();
+        MyCallable myCallable4 = new MyCallable();
+        MyCallable myCallable5 = new MyCallable();
+        MyCallable myCallable6 = new MyCallable();
+        MyCallable myCallable7 = new MyCallable();
+        MyCallable myCallable8 = new MyCallable();
 
         Future threadFuture = executorService.submit(myCallable);
+        Future threadFuture2 = executorService.submit(myCallable2);
+        Future threadFuture3= executorService.submit(myCallable3);
+        Future threadFuture4 = executorService.submit(myCallable4);
+        Future threadFuture5 = executorService.submit(myCallable5);
+        Future threadFuture6 = executorService.submit(myCallable6);
+        Future threadFuture7 = executorService.submit(myCallable7);
+        Future threadFuture8 = executorService.submit(myCallable8);
+
         try{
             Object resualt = threadFuture.get();
             System.out.println(resualt);
@@ -37,6 +58,9 @@ public class ThreadPoolExecutorDemo {
         } finally {
             executorService.shutdown();
         }
+        long end  = System.currentTimeMillis();
+
+        System.out.println(end - start);
 
     }
 }
